deb87c7fec7a2c027857f9d7e28ff9ed483ca2ae
Dominik Chrastecky <dominik@chrastecky.cz>
2026-01-23 12:26:24 +0100

    Fix currency code

--- spliit/shape/create_group.go
+++ spliit/shape/create_group.go
@@ -9,7 +9,7 @@ type ModifyGroupForm struct {
 	Name         string                   `json:"name"`
 	Information  *string                  `json:"information,omitempty"`
 	Currency     string                   `json:"currency"`
-	CurrencyCode *string                  `json:"currency_code,omitempty"`
+	CurrencyCode *string                  `json:"currencyCode,omitempty"`
 	Participants []ModifyGroupParticipant `json:"participants"`
 }
 
